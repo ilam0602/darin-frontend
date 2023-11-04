8487970c837cbc65b4106246cfdc7b7a8a34c034
ilam0602 <isaac.lam@live.com>
2023-11-03 21:41:16 -0700

    made webapp functional with all comps shown in wix

--- components/instructionsComponent/index.jsx
+++ components/instructionsComponent/index.jsx
@@ -1,6 +1,6 @@
 import styles from "./instructionsComponent.module.css";
 import { useContractRead, useContractWrite,useAccount } from 'wagmi';
-import { BigNumber,parseEther} from 'ethers';
+import { BigNumber,parseEther,formatEther} from 'ethers';
 import contractJson from '../../json/Pass.json';
 import React, { useState, useEffect } from 'react';
 
@@ -22,9 +22,9 @@ import { ConnectKitButton } from "connectkit";
 export default function InstructionsComponent() {
   const [isMounted, setIsMounted] = useState(false);
   const userAddress = useAccount();
-  // const contractAddress = "0xae906ed692429c064eD677Bb170640863DF5d88C"
-  const contractAddress = "0x2FaaceE3cC5f856FC0b297A7497875B67D320669"
+  const contractAddress = "0x5aB905fe512e3ee380B5FfaF42DE98F4167ca346"
   const deadAddress = "0x000000000000000000000000000000000000dEaD"
+  const [currQty,setCurrQty] = useState(1)
 
   const { data: balanceData } = useContractRead({
     address: contractAddress,
@@ -32,6 +32,7 @@ export default function InstructionsComponent() {
     functionName: 'balanceOf',
     args: userAddress.address ? [userAddress.address, 0]: [deadAddress,0],
   });
+
 
   const { data: prevCol } = useContractRead({
     address: contractAddress,
@@ -46,13 +47,25 @@ export default function InstructionsComponent() {
     functionName: 'isPassExpired',
     args: userAddress.address ? [userAddress.address,0] : [deadAddress,0],
   });
+  const { data: ownerAddress } = useContractRead({
+    address: contractAddress,
+    abi: contractJson.abi,
+    functionName: 'owner',
+  });
+
+  const { data: supplyData } = useContractRead({
+    address: contractAddress,
+    abi: contractJson.abi,
+    functionName: 'balanceOf',
+    args: ownerAddress ? [ownerAddress, 0]: [deadAddress,0],
+  });
 
 
   const { write: purchasePass } = useContractWrite({
     address: contractAddress,
     abi: contractJson.abi,
     functionName: 'purchasePass',
-    args: [0],
+    args: [0,currQty],
     onError(error) {
       console.log('Error', error)
     },
@@ -61,14 +74,27 @@ export default function InstructionsComponent() {
 
   const handleSubscribeClick = async () => {
     try {
-      await purchasePass({ value: sendEtherValue.toString() });
+      // let val = sendEtherValue.toString()
+      let val = parseEther((formatEther(sendEtherValue) * currQty).toString())
+      await purchasePass({ value:  val});
     } catch (error) {
       console.error("Error while trying to purchase pass:", error);
     }
   };
+
+  const handleInc = async () => {
+    setCurrQty(currQty + 1)
+
+  }
+  const handleDec = async () => {
+    if(currQty > 1){setCurrQty(currQty - 1)}
+
+  }
   
   useEffect(() => {
     setIsMounted(true);
+    console.log(ownerAddress);
+    console.log(supplyData)
   }, []);
 
   
@@ -76,6 +102,7 @@ export default function InstructionsComponent() {
  
 
   let displayBalance = isMounted ? (userAddress.address && (balanceData !== undefined) ? balanceData.toString() : "Wallet Not Connected") : "Loading..."; // Default text
+  let displaySupply = isMounted ? (ownerAddress && (supplyData !== undefined) ? supplyData.toString() : "Wallet Not Connected") : "Loading..."; // Default text
   let displayPrevCol = isMounted ? (userAddress.address && (prevCol!== undefined) ? prevCol.toString():"Wallet Not Connected" ) : "Loading..."; // Default text
   let displayExpPass = isMounted ? (userAddress.address && (expPass!== undefined )? expPass.toString():"Wallet Not Connected") : "Loading..."; // Default text
 
@@ -85,24 +112,38 @@ export default function InstructionsComponent() {
 
   return (
     <div className={styles.container}>
-      {(!userAddress.address && isMounted) ?
-      <ConnectKitButton/> :
-      <div>
-        <header className={styles.header_container}>
-          <div className={styles.header}>
-            <h1> balance: {displayBalance} </h1>
-            <h1> owns prev collection: {displayPrevCol} </h1>
-            <h1> pass expired: {displayExpPass} </h1>
-          </div>
-        </header>
-        <hr></hr>
-        <div>
-          <button onClick={handleSubscribeClick}>Mint</button> 
+    {(!userAddress.address && isMounted) ?
+    <ConnectKitButton/> :
+    <div>
+      <header className={styles.header_container}>
+        <div className={styles.header}>
+          {/* <hr></hr>
+          <p> Your Balance: {displayBalance} </p> 
+          <hr></hr>
+          <p> Supply Left: {displaySupply} </p>  */}
+          <hr></hr>
+          <p> Balance: {formatEther(sendEtherValue)} Eth</p> 
+          <hr></hr>
+          <p> Amount: </p> {/* Changed to <p> */}
+          <button onClick={handleDec}>-</button>
+          <p>{currQty}</p>
+          <button onClick={handleInc}>+</button>
+          {/* <hr></hr>
+          <p> Pass Expired: {displayExpPass} </p> */}
+          <hr></hr>
+          <p>Total: {(currQty * formatEther(sendEtherValue)).toFixed(5)} Eth</p>
         </div>
-        <ConnectKitButton/>
+      </header>
+      <hr></hr>
+      <div>
+        <button onClick={handleSubscribeClick}>Mint Now</button>
       </div>
-      }
+      <hr></hr>
+      <ConnectKitButton/>
+      <hr></hr>
     </div>
+    }
+  </div> 
   );
 }
 
